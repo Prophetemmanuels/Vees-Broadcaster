1708573eed2fc3e253a10fcf7de313cf232f0f1c
Prophetemmanuels <ghopbusinesscenter@gmail.com>
2026-02-04 11:01:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,9 @@
 self.addEventListener('install', e=>{
   e.waitUntil(
-    caches.open('vees-store').then(cache=>{
-      return cache.addAll(['index.html']);
-    })
+    caches.open('vees-thrift-v1').then(cache=>cache.addAll(['index.html']))
   );
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
 });
